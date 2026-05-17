9b8650021560ff93346f2c187cf118950a06d8c9
vvagridrones-create <vvagridrones@gmail.com>
2026-05-17 10:39:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ShopSphere from "../ShopSphere.jsx";
+import AdminDashboard from "../ShopSphere_Admin.jsx";
+
+const isAdmin = window.location.pathname === "/admin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ShopSphere />
+    {isAdmin ? <AdminDashboard /> : <ShopSphere />}
   </React.StrictMode>
 );
