16169d1440f05b3036d4b4e90b9492cda80a4a66
vvagridrones-create <vvagridrones@gmail.com>
2026-05-17 10:10:26 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ShopSphere from "../ShopSphere.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ShopSphere />
+  </React.StrictMode>
+);
